51330556ae6d18ef57acfc992fecbb387487566a
Alexander Tamarez <alextq3036@gmail.com>
2020-01-23 21:16:31 -0500

    Used the .env file and declared a MESSAGE_STYLE=uppercase variable, Then had the GET /json route handler transform the response object’s message to uppercase if process.env.MESSAGE_STYLE equals uppercase

--- BasicNodeAndExpress/myApp.js
+++ BasicNodeAndExpress/myApp.js
@@ -31,14 +31,22 @@ app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
 
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
-});
+//app.get("/json", (req, res) => {
+//  res.json({
+//    message: "Hello json"
+//  });
+//});
 
 /** 6) Use the .env file to configure the app */
  
+app.get("/json", function(req, res){
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({
+      "message": "hello json".toUpperCase()
+    });
+  }
+    res.json({"message": "Hello json"})
+})
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
